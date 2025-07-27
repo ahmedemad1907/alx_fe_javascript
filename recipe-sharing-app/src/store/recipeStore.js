9708f69ce8363773dcca9477ed82cfbc9fc49a96
Ahmed Emad <ahmedemad20021907@gmail.com>
2025-07-27 23:10:55 +0300

    Add searchTerm and setSearchTerm to recipeStore

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,19 +1,27 @@
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
-  recipes: [
-    { id: 1, title: 'Pizza', ingredients: ['cheese', 'tomato'], time: 30 },
-    { id: 2, title: 'Pasta', ingredients: ['pasta', 'sauce'], time: 20 },
-    { id: 3, title: 'Salad', ingredients: ['lettuce', 'cucumber'], time: 10 },
-  ],
+const useRecipeStore = create((set) => ({
+  recipes: [],
   searchTerm: '',
-  filteredRecipes: [],
-  setSearchTerm: (term) =>
-    set({ searchTerm: term }, false, 'setSearchTerm'),
-  filterRecipes: () =>
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  setRecipes: (recipes) => set({ recipes }),
+
+  addRecipe: (recipe) =>
+    set((state) => ({ recipes: [...state.recipes, recipe] })),
+
+  updateRecipe: (updatedRecipe) =>
     set((state) => ({
-      filteredRecipes: state.recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
+
+export default useRecipeStore;
